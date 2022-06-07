35cc4502721cf23ba37e5fa17bc59594c61a460b
laurenz fischbacher <laurenz.fischbacher@bulme.at>
2022-06-07 08:17:53 +0200

    update

--- forms - Kopie/delete/delete_reise.py
+++
@@ -1,5 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms.fields import IntegerField
-
-class DeleteForm(FlaskForm):
-    ReiseId = IntegerField("ReiseId")
--- forms - Kopie/delete/delete_reiseteilnehmer.py
+++
@@ -1,5 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms.fields import IntegerField
-
-class DeleteForm(FlaskForm):
-    ReisendeId = IntegerField("ReisendeId")
